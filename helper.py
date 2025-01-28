af52ff71a02765d64ef2c23f2dab69c283eea007
Jaykumar Patel <patel.jay4802@gmail.com>
2025-01-28 16:48:26 -0600

    update earnings calculation

--- helper.py
+++ helper.py
@@ -84,3 +84,101 @@ class DataRetriever:
         df_option_chain = df_option_chain[final_column_order]
 
         return date, df_option_chain
+    
+    def __getEarningsHistory(self):
+        url = f'https://www.alphavantage.co/query?function=EARNINGS&symbol={self.ticker}&apikey={st.secrets["ALPHA_VANTAGE_KEY"]}'
+        r = requests.get(url)
+        data = r.json()
+        quarterly_earnings = data["quarterlyEarnings"]
+        quarterly_earnings = quarterly_earnings[:min(7, len(quarterly_earnings))]
+        return [(entry["reportedDate"], entry["reportTime"]) for entry in quarterly_earnings]
+    
+    def __getDayData(self, date):
+        month = '-'.join(date.split('-')[:2])
+        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={self.ticker}&interval=30min&month={month}&outputsize=full&apikey={st.secrets["ALPHA_VANTAGE_KEY"]}'
+        r = requests.get(url)
+        data = r.json()
+        dates = list(data["Time Series (30min)"].keys())
+        position = "Middle"
+        if date in dates[0]:
+            position = "End"
+        elif date in dates[-1]:
+            position = "Beginning"
+        
+        open_price = data["Time Series (30min)"][f"{date} 09:30:00"]["1. open"]
+        close_price = data["Time Series (30min)"][f"{date} 16:00:00"]["4. close"]
+
+        return open_price, close_price, position
+    
+    def __getMonthEdgeData(self, month, edge):
+        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={self.ticker}&interval=30min&month={month}&outputsize=full&apikey={st.secrets["ALPHA_VANTAGE_KEY"]}'
+        r = requests.get(url)
+        data = r.json()
+        dates = list(data["Time Series (30min)"].keys())
+        if edge == "First":
+            date = dates[-1]
+        else:
+            date = dates[0]
+        
+        open_price = data["Time Series (30min)"][f"{date} 09:30:00"]["1. open"]
+        close_price = data["Time Series (30min)"][f"{date} 16:00:00"]["4. close"]
+
+        return open_price, close_price
+
+    def getEarningsDate(self):
+
+        earnings_history = self.__getEarningsHistory()
+
+        earnings_data = []
+
+        for reportedDate, reportTime in earnings_history:
+
+            price_before_earnings = None
+            price_after_earnings = None
+            
+            date_split = reportedDate.split('-')
+            current_date_open, current_date_close, position = self.__getDayData(reportedDate)
+            if (position == "Middle" and reportTime == "pre-market") or (position == "End" and reportTime == "pre-market"):
+                # get previous day data
+                previous_date = date_split[0] + '-' + date_split[1] + '-' + str(int(date_split[2]) - 1).zfill(2)
+                previous_date_open, previous_date_close, _ = self.__getDayData(previous_date)
+                price_before_earnings = previous_date_close
+                price_after_earnings = current_date_open
+            elif (position == "Middle" and reportTime == "post-market") or (position == "Beginning" and reportTime == "post-market"):
+                # get next day data
+                next_date = date_split[0] + '-' + date_split[1] + '-' + str(int(date_split[2]) + 1).zfill(2)
+                next_date_open, next_date_close, _ = self.__getDayData(next_date)
+                price_before_earnings = current_date_close
+                price_after_earnings = next_date_open
+            elif position == "Beginning":
+                # reaching here implies we need previous day's data (last day of previous month)
+                if int(date_split[1]) - 1 >= 1:
+                    previous_month = date_split[0] + '-' + str(int(date_split[1]) - 1).zfill(2)
+                else:
+                    previous_month = str(int(date_split[0]) - 1).zfill(2) + '-' + '12'
+                previous_date_open, previous_date_close = self.__getMonthEdgeData(previous_month, "Last")
+                price_before_earnings = previous_date_close
+                price_after_earnings = current_date_open
+            else:
+                # reaching here implies position is "End" and we need next day's data (first day of next month)
+                if int(date_split[1]) + 1 <= 12:
+                    next_month = date_split[0] + '-' + str(int(date_split[1]) + 1).zfill(2)
+                else:
+                    next_month= str(int(date_split[0]) - 1).zfill(2) + '-' + '01'
+                next_date_open, next_date_close = self.__getMonthEdgeData(next_month, "First")
+                price_before_earnings = current_date_close
+                price_after_earnings = next_date_open
+
+            percent_price_change = abs(((float(price_after_earnings) - float(price_before_earnings)) / float(price_before_earnings)) * 100)
+            earnings_data.append({
+                "reportedDate": reportedDate,
+                "reportTime": reportTime,
+                "priceBeforeEarnings": float(price_before_earnings),
+                "priceAfterEarnings": float(price_after_earnings),
+                "percentPriceChange": percent_price_change
+            })
+        
+        return earnings_data
+
+dr = DataRetriever("ibm")
+print(dr.getEarningsDate())
