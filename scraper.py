81bbaffc370dcf008e2c04221e341772d2761368
Jaykumar Patel <patel.jay4802@gmail.com>
2025-02-12 09:02:20 -0600

    scraper work

--- scraper.py
+++ scraper.py
@@ -4,7 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-tickers = ["AMD"]
+tickers = ["CVS", "D", "VRT", "EXC", "WAB", "KHC", "MLM", "GOLD", "SW", "WAT", "BAM", "QSR", "BIIB", "NI", "THC", "GMAB", "IPG", "LAD", "GNRC", "COOP", "R", "TMHC", "SITE", "ALKS", "CAMT", "BXMT", "SLVM", "UE", "SAH", "CHEF", "PAX", "DBD", "PX", "KRNT", "CIM", "RDWR", "HCSG", "AVXL", "CNDT", "SPTN", "MLYS", "GILT", "DENN", "BIOX", "OTLY", "VPG", "RDCM", "RGS", "PODC", "CRWS", "NMTC", "PYPD", "SPMC"]
+
 data = []
 
 for ticker in tickers:
@@ -16,20 +17,20 @@ for ticker in tickers:
         page_url = f"https://marketchameleon.com/Overview/{ticker}/Earnings/Earnings-Charts"
         driver.get(page_url)
 
-        # Extract elements
-        description_elements = driver.find_elements(By.CLASS_NAME, "symbol-section-header-descr")
-        description = description_elements[0].text
-        match = re.search(r"(\d+)%\s+of the time in the last (\d+)\s+quarters", description)
+    #     # Extract elements
+    #     description_elements = driver.find_elements(By.CLASS_NAME, "symbol-section-header-descr")
+    #     description = description_elements[0].text
+    #     match = re.search(r"(\d+)%\s+of the time in the last (\d+)\s+quarters", description)
 
-        if match:
-            percentage, quarters = match.group(1), match.group(2)
-        else:
-            percentage, quarters = None, None
+    #     if match:
+    #         percentage, quarters = match.group(1), match.group(2)
+    #     else:
+    #         percentage, quarters = None, None
 
     except:
         percentage, quarters = None, None
     
-    data.append((ticker, (percentage, quarters)))
+    # data.append((ticker, (percentage, quarters)))
 
 # Close the driver after the loop
 driver.quit()
