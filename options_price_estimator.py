1b19ae949b8a1a53e0fb6e2add56cc9a5228016a
Jaykumar Patel <patel.jay4802@gmail.com>
2025-02-19 14:18:42 -0600

    increase decimal places

--- options_price_estimator.py
+++ options_price_estimator.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-contract_price = st.number_input("Enter the contract price", value=0.0)
-vega = st.number_input("Enter the vega", value=0.0)
-theta = st.number_input("Enter the theta", value=0.0)
-current_iv = st.number_input("Enter the current IV", value=0.0)
-predicted_iv = st.number_input("Enter the predicted IV", value=0.0)
+contract_price = st.number_input("Enter the contract price", value=0.0, format="%.5f")
+vega = st.number_input("Enter the vega", value=0.0, format="%.5f")
+theta = st.number_input("Enter the theta", value=0.0, format="%.5f")
+current_iv = st.number_input("Enter the current IV", value=0.0, format="%.5f")
+predicted_iv = st.number_input("Enter the predicted IV", value=0.0, format="%.5f")
 
 if contract_price and vega and theta and current_iv and predicted_iv:
     # Calculate the value based on the inputs
